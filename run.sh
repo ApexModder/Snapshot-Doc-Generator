6c03e31e312067b243335436e6509e916dde5a07
Apex <robsonlawson3@gmail.com>
2026-03-23 21:29:42 +0000

    Fix outdated args in run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env sh
 
-# Option                                    Description
-# ------                                    -----------
-# -?, -h, --help                            Shows the help menu
-# --all [ReleaseType]                       Generate all known versions (with optional filter)
-# -g, --generator [Generator]               Type of generator to run
-#                                           [generic, json, neoforge, forgecraft]
-# -l, --list [ReleaseType]                  List all known versions (with optional filter)
-# -o, --out <Path>                          Directory to store generated files (default './out')
-# -v, --version <Id>                        Version id to generate
-# --versions <File>                         Custom version(s).json file
+# Option                    Description
+# ------                    -----------
+# -?, -h, --help            Shows the help menu
+# --all                     Generate all known versions
+# -g, --generator <String>  Generator type to run (default: generic)
+# -l, --list                List all known versions
+# -o, --out <Path>          Directory to store generated files (default: out)
+# -v, --version <String>    Version id to process
+# --versions <File>         Custom versions.json file
 
 # Generator: [ generic, json, neoforge, forgecraft ]
 # ReleaseType: [ RELEASE, RELEASE_CANDIDATE, PRE_RELEASE, SNAPSHOT ]
